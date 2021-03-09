7a22135e6ccb3cf11082fc984557628603910390
Regan-Mclachlan <reganmc96@hotmail.com>
2021-03-09 19:52:06 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+
+const cards = ['Lisa', 'Kaitlin', 'Jan']
+
+ function writeCards(cards){
+let thankYouCards = [];
+for (let i = 0; i < cards.length; i++) {
+    thankYouCards.push (`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+}
+return thankYouCards
+ }
+const number = 10
+function countDown(number){
+while (number >= 0 ) console.log(number--);}
